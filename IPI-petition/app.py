5e3274a6f8622ce01c3f99678209265b43b3fc73
YangIon <yyangc88@gmail.com>
2019-02-07 20:27:13 -0500

    Flask petition base framework

---
+++ IPI-petition/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, url_for, abort, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\Github\\IPI-petition\\test.db'
+db = SQLAlchemy(app)
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        self.name = self.name.replace("'", "")
+        return '<User %r, %s>' % (self.name, self.email)
+
+petitionGroup = []
+
+@app.route("/")
+def index():
+    petitionGroup = Person.query.all()
+    return render_template("index.html", petition=petitionGroup)
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    name_input = request.form['name']
+    email_input = request.form['email']
+    if name_input == "" or email_input == "":
+        abort(401)
+
+    person = Person(name=name_input, email=email_input)
+    db.session.add(person)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+    
+    
