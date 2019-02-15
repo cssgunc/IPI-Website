9de8d0d8d9382097d2c0879040b51fd917d7dff2
YangIon <yyangc88@gmail.com>
2019-02-14 22:19:16 -0500

    Expanded database and better HTML framework

--- IPI-petition/app.py
+++ IPI-petition/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, url_for, abort, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\Github\\IPI-petition\\test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///D:\\Github\\IPI-Website\\IPI-petition\\test.db'
 db = SQLAlchemy(app)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
+    year = db.Column(db.Integer, unique=False, nullable=False)
+    comment = db.Column(db.String, unique = False, nullable=True)
 
     def __repr__(self):
         self.name = self.name.replace("'", "")
@@ -25,10 +27,12 @@ def index():
 def add():
     name_input = request.form['name']
     email_input = request.form['email']
-    if name_input == "" or email_input == "":
+    year_input = request.form['year']
+    comment_input = request.form['comment']
+    if name_input == "" or email_input == "" or year_input == "":
         abort(401)
 
-    person = Person(name=name_input, email=email_input)
+    person = Person(name=name_input, email=email_input, year=year_input, comment=comment_input)
     db.session.add(person)
     db.session.commit()
     return redirect(url_for('index'))
