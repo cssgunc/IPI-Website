62303df012d490282b38793689d5285c00f2dfd1
YangIon <yyangc88@gmail.com>
2019-04-11 18:44:28 -0400

    IPI Petition PHP

---
+++ IPI-petition-php/public/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Simple Database App</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1>Simple Database App</h1>
